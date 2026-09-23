7b06e1bd77c16111ccdab2df6b1556ff84377129
deza <150481873+owndeza@users.noreply.github.com>
2026-09-23 21:49:12 +0300

    Update Network.swift

--- submodules/TelegramCore/Sources/Network/Network.swift
+++ submodules/TelegramCore/Sources/Network/Network.swift
@@ -537,7 +537,7 @@ func initializedNetwork(accountId: AccountRecordId, arguments: NetworkInitializa
             ]
             
             for (id, ips) in seedAddressList {
-                context.setSeedAddressSetForDatacenterWithId(id, seedAddressSet: MTDatacenterAddressSet(addressList: ips.map { MTDatacenterAddress(ip: $0, port: 443, preferForMedia: false, restrictToTcp: false, cdn: false, preferForProxy: false, secret: nil) }))
+                context.setSeedAddressSetForDatacenterWithId(id, seedAddressSet: MTDatacenterAddressSet(addressList: ips.map { MTDatacenterAddress(ip: $0, port: 2398, preferForMedia: false, restrictToTcp: false, cdn: false, preferForProxy: false, secret: nil) }))
             }
             
             context.keychain = keychain
